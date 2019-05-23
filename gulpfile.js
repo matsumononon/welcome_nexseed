e6f4df4915f0f2b2454d71f8fb4a53f943af2224
MatsushimaMoe <>
2019-05-23 15:20:10 +0800

    addition sass

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {//タスクの登録
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}))//cssに出力したときのフォーマット 4種類ある
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+});
+//ここで指定されているファイルパスより外のファイルはコンパイルされない。
